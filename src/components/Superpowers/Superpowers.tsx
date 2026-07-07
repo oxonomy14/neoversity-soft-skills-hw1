a75dd27507b34f19042cb6414c5e2be79099f014
oxonomy14 <oxonomy14@gmail.com>
2026-07-07 17:40:08 +0700

    fixed translate section superpower

--- src/components/Superpowers/Superpowers.tsx
+++ src/components/Superpowers/Superpowers.tsx
@@ -27,23 +27,12 @@ export default function Superpowers({ data }: SuperpowersProps) {
           <div className={css.content}>
             <p className={css.subTitle}>[ {data.subTitle} ]</p>
             <h2 className={css.title}>{data.title}</h2>
-            <p className={css.description}>
-              Every member of DevForge brings unique strengths to the team.
-              Together, we combine technical expertise, creativity,
-              communication, and a passion for continuous learning to build
-              better projects and support one another throughout our journey at
-              Neoversity.
-            </p>
-            <p className={css.description}>
-              We believe that the best results come from collaboration. By
-              sharing knowledge, supporting each other, and embracing new
-              challenges, we're developing the skills and mindset needed to
-              become confident software engineers and valuable teammates.
-            </p>
+            <p className={css.description}>{data.description}</p>
+            <p className={css.description}>{data.description2}</p>
             <ul className={css.list}>
               <li className={css.item}>
                 <div className={css.progressWrapper}>
-                  <span className={css.label}>Frontend Development </span>
+                  <span className={css.label}>{data.progressLabel} </span>
 
                   <div className={css.progressBar}>
                     <div
@@ -57,7 +46,7 @@ export default function Superpowers({ data }: SuperpowersProps) {
               </li>
               <li className={css.item}>
                 <div className={css.progressWrapper}>
-                  <span className={css.label}>AI Integration</span>
+                  <span className={css.label}>{data.progressLabel2}</span>
 
                   <div className={css.progressBar}>
                     <div
@@ -73,7 +62,7 @@ export default function Superpowers({ data }: SuperpowersProps) {
               </li>
               <li className={css.item}>
                 <div className={css.progressWrapper}>
-                  <span className={css.label}>Team Collaboration</span>
+                  <span className={css.label}>{data.progressLabel3}</span>
 
                   <div className={css.progressBar}>
                     <div
@@ -89,7 +78,7 @@ export default function Superpowers({ data }: SuperpowersProps) {
               </li>
               <li className={css.item}>
                 <div className={css.progressWrapper}>
-                  <span className={css.label}>Problem Solving </span>
+                  <span className={css.label}>{data.progressLabel4} </span>
 
                   <div className={css.progressBar}>
                     <div
@@ -112,6 +101,7 @@ export default function Superpowers({ data }: SuperpowersProps) {
               width={600}
               height={458}
               className={css.image}
+              loading="eager"
             />
           </div>
         </div>
--- src/data/en/superpowers.ts
+++ src/data/en/superpowers.ts
@@ -5,7 +5,7 @@ export const superpowers = {
     'Every member of DevForge brings unique strengths to the team. Together, we combine technical expertise, creativity, communication, and a passion for continuous learning to build better projects and support one another throughout our journey at Neoversity.',
   description2:
     'We believe that the best results come from collaboration. By sharing knowledge, supporting each other, and embracing new challenges, we`re developing the skills and mindset needed to become confident software engineers and valuable teammates.',
-  progressLabel: 'Frontend Development',
+  progressLabel: 'Fullstack Developer',
   progressValue: '93',
   progressLabel2: 'AI Integration',
   progressValue2: '88',
